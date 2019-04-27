e26f7cd8b142367513a2e78e3aee82919642b7ef
Caelan Garrett <caelan@mit.edu>
2019-04-26 20:44:08 -0400

    Pruning infeasible queries

--- extrusion/run.py
+++ extrusion/run.py
@@ -10,24 +10,27 @@ import numpy as np
 import argparse
 import time
 import random
+import heapq
 
 from itertools import permutations
 
 from extrusion.extrusion_utils import TOOL_NAME, load_world, \
     get_node_neighbors, get_disabled_collisions, MotionTrajectory, PrintTrajectory, is_ground, \
-    get_supported_orders, element_supports, is_start_node, doubly_printable, retrace_supporters, get_element_neighbors, get_other_node
+    get_supported_orders, element_supports, is_start_node, doubly_printable, retrace_supporters, get_element_neighbors, \
+    get_other_node
 from extrusion.parsing import load_extrusion, draw_element, create_elements, get_extrusion_path, draw_model
 from examples.pybullet.utils.pybullet_tools.utils import connect, disconnect, wait_for_interrupt, \
     get_movable_joints, set_joint_positions, link_from_name, add_line, get_link_pose, wait_for_duration, add_text, \
-    plan_joint_motion, point_from_pose, get_joint_positions, LockRenderer, wait_for_user, has_gui, remove_all_debug
+    plan_joint_motion, point_from_pose, get_joint_positions, LockRenderer, wait_for_user, has_gui, \
+    remove_all_debug, ClientSaver
 from extrusion.stream import get_print_gen_fn, get_wild_print_gen_fn, test_stiffness, SELF_COLLISIONS
-
 
 from pddlstream.algorithms.focused import solve_focused
 from pddlstream.language.constants import PDDLProblem, And, print_solution
 from pddlstream.language.generator import from_test
 from pddlstream.language.stream import StreamInfo, PartialInputs
-from pddlstream.utils import read, get_file_path, user_input, neighbors_from_orders, INF, elapsed_time, get_connected_components
+from pddlstream.utils import read, get_file_path, user_input, neighbors_from_orders, INF, \
+    elapsed_time, get_connected_components
 
 
 JOINT_WEIGHTS = [0.3078557810844393, 0.443600199302506, 0.23544367607317915,
@@ -340,25 +343,43 @@ def heuristic_planner(robot, obstacles, node_points, element_bodies, ground_node
     print_gen_fn = get_print_gen_fn(robot, obstacles, node_points, element_bodies, ground_nodes,
                                     precompute_collisions=False, **kwargs)
     node_neighbors = get_node_neighbors(element_bodies)
+
+    connected_nodes = set()
+    queue = []
+    queued = set()
+    def add_node(node):
+        connected_nodes.add(node)
+        for element in node_neighbors[node]:
+            if element not in queued:
+                score = max(node_points[n][2] for n in element)
+                heapq.heappush(queue, (score, element))
+                queued.add(element)
+
+    for node in ground_nodes:
+        add_node(node)
     extruded_elements = set()
     planned_trajectories = []
-    connected_nodes = set(ground_nodes)
-    print(node_points)
-    # Using max does not work at this point because of tiebreaking
-    for element in sorted(element_bodies, key=lambda e: min(node_points[n][2] for n in e)): # TODO: tiebreak by angle or x:
-        # TODO: select from actually applicable
-        # TODO: handle tiebreaking in a nice way
-        print(element, connected_nodes)
-        print([node_points[n] for n in element])
+    while queue:
+        _, element = heapq.heappop(queue)
+        # TODO: tiebreak by angle or x
         node = random.choice([n for n in element if n in connected_nodes])
         try:
             trajectory, = next(print_gen_fn(node, element, extruded=extruded_elements))
         except StopIteration:
-            print('Failure!')
+            client = connect(use_gui=True)
+            with ClientSaver(client):
+                floor, robot = load_world()
+                handles = []
+                for e in extruded_elements:
+                    handles.append(draw_element(node_points, e, color=(0, 1, 0)))
+                handles.append(draw_element(node_points, element, color=(1, 0, 0)))
+                print('Failure!')
+                wait_for_user()
             break
         planned_trajectories.append(trajectory)
         extruded_elements.add(element)
-        connected_nodes.update(element)
+        for node in element:
+            add_node(node)
     return planned_trajectories
 
 ##################################################
