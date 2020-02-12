e6d88cc60a7398dec1785787d496c4dd8641eb35
caelan <caelan@mit.edu>
2020-02-12 10:18:13 -0500

    Visualization

--- extrusion/stiffness.py
+++ extrusion/stiffness.py
@@ -13,7 +13,7 @@ from pyconmech import StiffnessChecker
 from extrusion.utils import get_extructed_ids, compute_sequence_distance, get_distance, compute_printable_directed, get_undirected, \
     get_pairs, compute_z_distance, reverse_element
 from pddlstream.utils import get_connected_components
-from pybullet_tools.utils import HideOutput, INF, elapsed_time, wait_for_user, BLUE, RED
+from pybullet_tools.utils import HideOutput, INF, elapsed_time, wait_for_user, BLUE, RED, BLACK
 
 TRANS_TOL = 0.0015
 ROT_TOL = INF # 5 * np.pi / 180
@@ -118,12 +118,13 @@ def solve_tsp(elements, ground_nodes, node_points, initial_point, max_time=30, v
     # TODO: reuse by simply swapping out the first vertex
     from ortools.constraint_solver import routing_enums_pb2
     from ortools.constraint_solver import pywrapcp
-    from extrusion.visualization import draw_ordered
+    from extrusion.visualization import draw_ordered, draw_model
     assert initial_point is not None
     start_time = time.time()
     nodes = {node for element in elements for node in element}
-    point_from_node, edges = embed_graph(elements, node_points, ground_nodes, initial_point, num=2)
-    edges.update({pair for pair in product(nodes, repeat=2)})
+    point_from_node, frame_edges = embed_graph(elements, node_points, ground_nodes, initial_point, num=2)
+    transit_edges = {pair for pair in product(nodes, repeat=2)}
+    edges = frame_edges | transit_edges
 
     node_from_index = list(point_from_node)
     index_from_node = dict(map(reversed, enumerate(node_from_index)))
@@ -139,8 +140,14 @@ def solve_tsp(elements, ground_nodes, node_points, initial_point, max_time=30, v
         i1, i2 = index_from_node[n1], index_from_node[n2]
         p1, p2 = point_from_node[n1], point_from_node[n2]
         distance = get_distance(p1, p2)
-        scale = 1 if directed in edges else PENALTY
-        distance_from_node[i1, i2] = int(math.ceil(SCALE*scale*distance))
+        #if directed in elements:
+        #    scale =
+        if directed in edges:
+            scale = 1
+        else:
+            scale = PENALTY
+        weight = int(math.ceil(SCALE*scale*distance))
+        distance_from_node[i1, i2] = min(weight, distance_from_node.get((i1, i2), INF))
 
     # def time_callback(from_index, to_index):
     #     """Returns the travel time between the two nodes."""
@@ -181,8 +188,8 @@ def solve_tsp(elements, ground_nodes, node_points, initial_point, max_time=30, v
     if not assignment:
         print('Failure! Duration: {:.3f}s'.format(elapsed_time(start_time)))
         return None
-    print('Success! Objective: {:.3f}, Duration: {:.3f}s'.format(
-        assignment.ObjectiveValue() / SCALE, elapsed_time(start_time)))
+    print('Success! Vertices: {} | Edges: {} | Objective: {:.3f}, Duration: {:.3f}s'.format(
+        len(node_from_index), len(edges), assignment.ObjectiveValue() / SCALE, elapsed_time(start_time)))
     index = solver.Start(0)
     order = []
     while not solver.IsEnd(index):
@@ -193,10 +200,14 @@ def solve_tsp(elements, ground_nodes, node_points, initial_point, max_time=30, v
     #order.append(node_from_index[manager.IndexToNode(index)])
     start = order.index(INITIAL_NODE)
     order = order[start:] + order[:start] + [INITIAL_NODE]
+    print(order)
     # TODO: penalize downwards printing directions by z or phi
 
-    print(order)
-    draw_ordered(get_pairs(order), point_from_node)
+    # TODO: visualize by weight
+    tour = get_pairs(order)
+    #draw_model(elements, point_from_node, ground_nodes, color=BLACK)
+    draw_model(frame_edges - set(tour), point_from_node, ground_nodes, color=BLACK)
+    draw_ordered(tour, point_from_node)
     wait_for_user()
 
     return order
@@ -239,7 +250,7 @@ def embed_graph(elements, node_points, ground_nodes, initial_position=None, num=
             path.append(n3)
         path.append(n2)
         edges.update(get_pairs(path))
-        edges.update(reversed(list(get_pairs(path))))
+        edges.update(get_pairs(list(reversed(path))))
 
     if initial_position is not None:
         point_from_vertex[INITIAL_NODE] = initial_position
